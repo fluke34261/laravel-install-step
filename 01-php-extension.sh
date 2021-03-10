e9a258312b43a450b31f1cd5ab0c9ad4dc957332
TheMegaNear <fluke34261@users.noreply.github.com>
2021-03-10 15:31:32 +0700

    Update 01-php-extension.sh

--- 01-php-extension.sh
+++ 01-php-extension.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install -y php7.4-{curl,xml,fpm,bcmath,bz2,intl,gd,mbstring,mysql,zip,common}
+sudo apt-get install -y "php7.4-{curl,xml,fpm,bcmath,bz2,intl,gd,mbstring,mysql,zip,common}"

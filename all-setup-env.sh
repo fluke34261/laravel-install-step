05da14164e84992c6013ec8d3f73bdd5a024a518
TheMegaNear <fluke34261@users.noreply.github.com>
2021-04-16 18:02:34 +0700

    Update all-setup-env.sh

--- all-setup-env.sh
+++ all-setup-env.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt update
+
 # install php and php-extension
 sudo apt install -y php7.4 php7.4-curl php7.4-xml php7.4-bcmath php7.4-bz2 php7.4-intl php7.4-gd php7.4-mbstring php7.4-mysql php7.4-zip php7.4-common
 

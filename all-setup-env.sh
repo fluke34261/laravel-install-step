763f52df03721ea5ff832a3ec7d1154a892e8902
TheMegaNear <fluke34261@users.noreply.github.com>
2023-03-17 10:42:28 +0700

    Update all-setup-env.sh

--- all-setup-env.sh
+++ all-setup-env.sh
@@ -3,6 +3,8 @@
 sudo apt update --fix-missing
 
 # install php and php-extension
+sudo apt update && apt install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
 
 #version 7 default
 #sudo apt install -y php7.4 php7.4-fpm php7.4-curl php7.4-xml php7.4-bcmath php7.4-bz2 php7.4-intl php7.4-gd php7.4-mbstring php7.4-mysql php7.4-zip php7.4-common
